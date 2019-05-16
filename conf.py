912bfdc988a8d558c152dcc8a1ef5147e54090de
subu.bhagavati <subu.bhagavati@avangate.com>
2019-05-17 02:11:04 +0300

    Updated code for running crons.

--- conf.py
+++ conf.py
@@ -3,3 +3,9 @@ Chrome = r"C:\Tools\chromedriver26_win32\chromedriver.exe"
 Username = "cc"
 Password = "Cc#"
 Url = "http://sandbox8.avangate.local/admin"
+
+CRON_HOST = "farm03.avangate.local"
+CRON_USER = "subub"
+CRON_PATH = "websites/sandbox83.avangate.local/htdocs/cron/"
+
+
--- core/Cron.py
+++ core/Cron.py
@@ -1,34 +1,41 @@
 import paramiko
 from crontab import CronTab
-import array
-from core.config import getConfig
+import conf
+import warnings
 
 
 class CronRun:
+    #TODO: clean this up for local and remote cron connections
     def __init__(self):
-        if getConfig("CRON_HOST") != 'localhost':
+        if conf.CRON_HOST != 'localhost':
             self.client = paramiko.SSHClient()
             self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            self.client.connect(getConfig("CRON_HOST"), 22, self.getConfig("USER"), self.getConfig('key'))
+            # TODO: Clean this up.
+            warnings.filterwarnings(action='ignore', module='.*paramiko.*')
+            self.client.connect(conf.CRON_HOST, 22, conf.CRON_USER, allow_agent=True)
+            self.run_cron_remote('notify_delivery.php')
 
-    def runcron(self, cronname):
-        if getConfig("CRON_HOST") == 'localhost':
-            return self.runcronlocal(cronname)
+    def run_cron(self, cronname):
+        if conf.CRON_HOST == 'localhost':
+            return self.run_cron_local(cronname)
         else:
-            return self.runcronremote(cronname)
+            return self.run_cron_remote(cronname)
 
-    def runcronlocal(self, cronname):
-        sudo = ''
-        if getConfig('CRON_USER'):
-            cron = CronTab(getConfig('CRON_USER'))
-            job = cron.new('sudo -u ' + getConfig('CRON_USER') + ' /usr/bin/php ' + getConfig(
-                'CRON_PATH') + cronname + ' 2>/dev/null')
+    # TODO: get the execution text.
+    def run_cron_local(self, cronname):
+        if conf.CRON_USER:
+            cron = CronTab(conf.CRON_USER)
+            cron.new('sudo -u ' + conf.CRON_USER + ' /usr/bin/php ' + conf.CRON_PATH + cronname + ' 2>/dev/null')
             return cron.write()
 
-    def runcronremote(self, cronname):
-        data = array.array()
-        stdin, stdout, stderr = self.client.exec_command("runcron " + getConfig('CRON_PATH') + cronname)
+    def run_cron_remote(self, cron_name):
+        data = []
+        stdin, stdout, stderr = self.client.exec_command("runcron " + conf.CRON_PATH + cron_name + " accounts:21317")
         for line in stdout:
             data.append(line.strip('\n'))
         self.client.close()
+        print(data)
         return data
+
+
+sshConn = CronRun()
