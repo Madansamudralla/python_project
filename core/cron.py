0e5a6b7a8657ea353d9a03ced90bf640dfa93bf8
petrisor.ureche <petrisor.ureche@2checkout.com>
2019-05-22 15:19:52 +0300

    review code structure

--- core/cron.py
+++ core/cron.py
@@ -11,9 +11,11 @@ class CronRun:
                 self.client = paramiko.SSHClient()
                 self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
                 warnings.filterwarnings(action='ignore', module='.*paramiko.*')
-                self.client.connect(config_automation.Config.CRON_HOST, 22, config_automation.Config.CRON_USER, allow_agent=True)
+                self.client.connect(config_automation.Config.CRON_HOST, 22, config_automation.Config.CRON_USER,
+                                    allow_agent=True)
             except paramiko.AuthenticationException:
-                raise ConnectionError('Could not connect via SSH Client Make sure you have the proper user in the "config_automation.py" file and that your SSH agent is opened.')
+                raise ConnectionError('Could not connect via SSH Client Make sure you have the proper user in the '
+                                      '"config_automation.py" file and that your SSH agent is opened.')
 
     def run_cron(self, cron_name):
         if config_automation.Config.CRON_HOST == 'localhost':
@@ -21,13 +23,15 @@ class CronRun:
         else:
             return self.run_cron_remote(cron_name)
 
-
     # TODO: Clean debugging prints after core is implemented
+
     def run_cron_local(self, cron_name):
         data = []
         try:
             if config_automation.Config.CRON_USER:
-                stdin, stdout, stderr = self.client.exec_command('sudo -u ' + config_automation.Config.CRON_USER + ' /usr/bin/php ' + config_automation.Config.CRON_PATH + cron_name + ' 2>/dev/null')
+                stdin, stdout, stderr = self.client.exec_command('sudo -u ' + config_automation.Config.CRON_USER
+                                                                 + ' /usr/bin/php ' + config_automation.Config.CRON_PATH
+                                                                 + cron_name + ' 2>/dev/null')
                 for line in stdout:
                     data.append(line.strip('\n'))
         finally:
@@ -35,16 +39,17 @@ class CronRun:
             print(data)
         return data
 
-    #TODO: Clean debugging prints after core is implemented
+    # TODO: Clean debugging prints after core is implemented
+
     def run_cron_remote(self, cron_name):
         data = []
         try:
-            stdin, stdout, stderr = self.client.exec_command("runcron " + config_automation.Config.CRON_PATH + cron_name)
+            stdin, stdout, stderr = self.client.exec_command("runcron " +
+                                                             config_automation.Config.CRON_PATH + cron_name)
             for line in stdout:
                 data.append(line.strip('\n'))
         finally:
             self.client.close()
             print(data)
         return data
-
 
