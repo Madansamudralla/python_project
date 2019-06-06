5a5f5554504c8812fd9e8298fbfc1e109281d338
petrisor.ureche <petrisor.ureche@2checkout.com>
2019-06-06 11:33:38 +0300

    Refactor driver instance to open and close after scenario

--- features/steps/convert_plus.py
+++ features/steps/convert_plus.py
@@ -1,10 +1,6 @@
 from behave import *
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from features.convert_plus.page_model.buy_link_through_api import ApiBuyLink
-from features.convert_plus.locators.checkout_page import CheckoutPageLocators
+from features.convert_plus.page_model.buy_link_through_api import BuyLinkThroughApi
 from features.convert_plus.page_model.checkout_page import CheckoutPage
-import time
 
 use_step_matcher("re")
 
@@ -12,24 +8,20 @@ use_step_matcher("re")
 @given('I am in the ConvertPlus default "(.*)" checkout page with one regular product')
 def step_impl(context, template):
     """
+    :param template:
+    :type template: str
     :type context: behave.runner.Context
     """
-    response = ApiBuyLink()
-    if template == "default":
-        url = response.buy_link_with_one_regular_product_on_default_template()
-    elif template == "one-column":
-        url = response.buy_link_with_one_regular_product_on_one_column_template()
-    context.browser.get(url)
-
+    get_base_url = BuyLinkThroughApi()
+    get_base_url.get_on_base_url(template)
 
 @step("I wait for checkout page to load")
 def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    WebDriverWait(context.browser, 30).until(
-        EC.visibility_of_element_located(CheckoutPageLocators.PLACE_ORDER_BUTTON_LOCATOR)
-    )
+    wait = CheckoutPage()
+    wait.wait_for_checkout_page_to_load()
 
 
 @when("I fill the billing details")
@@ -37,7 +29,7 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    checkout = CheckoutPage(context.browser)
+    checkout = CheckoutPage()
     checkout.fill_country('Netherlands')
     checkout.wait_for_loader_to_disappear()
 
@@ -47,7 +39,7 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    checkout = CheckoutPage(context.browser)
+    checkout = CheckoutPage()
     checkout.fill_email_address('cp_behave@2checkout.com')
     checkout.fill_cc_details('4111 1111 1111 1111', '12/22', '123')
 
@@ -57,7 +49,7 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    checkout = CheckoutPage(context.browser)
+    checkout = CheckoutPage()
     checkout.click_on_place_order()
 
 
@@ -66,6 +58,5 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    checkout = CheckoutPage(context.browser)
+    checkout = CheckoutPage()
     checkout.the_finish_page_is_fully_loaded()
-
