1a9f45f7304d4b7b46c94497ce2b016f4fdd76de
petrisor.ureche <petrisor.ureche@2checkout.com>
2019-07-02 12:35:20 +0300

    First refactor of framework

--- features/convert_plus/page_model/thank_you_page.py
+++ features/convert_plus/page_model/thank_you_page.py
@@ -6,7 +6,7 @@ class ThankYouPage:
     TIME_SEC = 5
 
     def __init__(self):
-        self.driver = core.get({"host": "localhost", 'port': 4444}, delegator="chrome").driver
+        self.driver = core.get(core.res['chrome'], feature="browser")._res.driver.webdriver
 
     def get_reference_number_from_finish_page(self):
         elements = self.driver.find_elements(*CheckoutPageLocators.FINISH_PAGE_REFERENCE_NUMBER_LOCATOR)
